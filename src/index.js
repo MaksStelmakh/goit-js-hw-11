df5004f55c56a0bd42153363b9e1a75283a9dde0
MaksStelmakh <step4ick2017@gmail.com>
2021-11-22 19:43:27 +0200

    Виправив помилки з відловом помилок

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import axios from "axios";
+// import axios from "axios";
 import Notiflix from 'notiflix';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import debounce from "lodash.debounce"
@@ -21,8 +21,8 @@ function infiniteScroll() {
   if (clientHeight + scrollTop >= scrollHeight - 5) {
     loadMore.classList.add(`show`)
     page += 1
-    getPhoto().then(createGallery)
-
+    getPhoto().then(createGallery).catch(error => console.log(error))
+    return
   }
 }
 
@@ -39,7 +39,7 @@ function onSearch(evt) {
     return
   }
   page = 1
-  getPhoto().then(createGallery)
+  getPhoto().then(createGallery).catch(error => console.log(error))
 
 }
 
@@ -50,22 +50,37 @@ function getPhoto() {
     alert(`The pictures are over`);
     return
   }
-    return axios.get(`${BASE_URL}${SETTINGS}&per_page=40&page=${page}&q=${searchForm}`);
+  return fetch(`${BASE_URL}${SETTINGS}&per_page=40&page=${page}&q=${searchForm}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Error fetching data`)
+      }
+      return response.json()
+    })
 }
 
+// Запрос не бэк через Axios
+// function getPhoto() {
+//   const BASE_URL = `https://pixabay.com/api/?key=24435694-017d2bab3470121913608c0c0`
+//   const SETTINGS = `&image_type=photo&orientation=horizontal&safesearch=true`
+//   if (page >= 13) {
+//     alert(`The pictures are over`);
+//     return
+//   }
+//     return axios.get(`${BASE_URL}${SETTINGS}&per_page=40&page=${page}&q=${searchForm}`);
+// }
 
 function createGallery(resp) {
   console.log(resp)
-  
-  if (resp.data.totalHits === 0) {
+  if (resp.totalHits === 0) {
     Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
     gallery.innerHTML = ``
     return
   } else if (page === 1) {
-    Notiflix.Notify.success(`Hooray! We found ${resp.data.totalHits} images.`);
+    Notiflix.Notify.success(`Hooray! We found ${resp.totalHits} images.`);
   } 
   loadMore.classList.remove(`show`)
-  return resp.data.hits.map(({ webformatURL, likes, views, comments, downloads, tags, largeImageURL }) => {
+  return resp.hits.map(({ webformatURL, likes, views, comments, downloads, tags, largeImageURL }) => {
             return gallery.insertAdjacentHTML(`beforeend`, articles({ webformatURL, likes, views, comments, downloads, tags, largeImageURL }))
         }).join("")
         }    
