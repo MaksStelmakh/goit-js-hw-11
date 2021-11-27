139744d63657ded2ad8d6c870c1ce7a5d808d065
MaksStelmakh <step4ick2017@gmail.com>
2021-11-27 10:56:40 +0200

    Внёс правки в разметку

--- src/articles.js
+++ src/articles.js
@@ -1,6 +1,7 @@
 export function articles({ webformatURL, likes, views, comments, downloads, tags, largeImageURL }) {
   return `<li><div class="image-box"><a href="${largeImageURL}">
           <img class="img-hover" src="${webformatURL}" alt="${tags}" loading="lazy" width="400px" height="300px">
+          </a>
           <ul class="about-img">
           <li class="name-numbers">Likes
             <p class="number">${likes}</p>
@@ -13,6 +14,5 @@ export function articles({ webformatURL, likes, views, comments, downloads, tags
           </p></li>
           <li class="name-numbers">Downloads<p class="number">${downloads}</p></li>
         </ul>
-        </a>
         </div></li>`
 }
